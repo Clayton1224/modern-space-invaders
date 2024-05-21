46c8ed95b60aa53432af3951a513ccd6b329459c
Clayton1224 <claytonrichard153@gmail.com>
2024-05-21 17:22:29 -0400

    class power up

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -54,4 +54,24 @@ update() {
 
             });
         }
+        class powerUP{
+            constructor({position, velocity}) {
+            this.position = position;
+            this.velocity = velocity;
+            this.radius = 15;
+
+        }
+        }
+        draw() {
+            c.beginPath();
+            c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+            c.closePath();
+            c.fillStyle = this.color;
+            c.fill();
+            c
+        }
+        update() {
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+        }
     
