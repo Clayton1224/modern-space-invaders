8638cdeee044eb5f0da87004c8e5314a67101029
Clayton1224 <claytonrichard153@gmail.com>
2024-05-23 13:53:17 -0400

    add update function

--- js/classes/grid.js
+++ js/classes/grid.js
@@ -1,33 +1,45 @@
-class grid{
- constructor(){
- }
-}
-  this.position ={
-    x: 0,
-    y: 0,
-  };
-  this.velocity ={
-    x: 3,
-    y: 0,
-  };
-  this.invaders = [];
-
-  const columns = Math.floor(Math.random()* 10 + 5);
-  const rows = Math.floor(Math.random()* 10 + 2);
-  this.whidth = columns * 30;
-
-   for(let i = 0; i < columns; i++){
-    for(let j = 0; j < rows; j++) { 
-        this.invaders.push{new invader()}
-    
-        i: j, * 30,
-        j: j * 30,
-
+class Grid {
+    constructor() {
+      this.position = {
+        x: 0,
+        y: 0,
+      };
+      this.velocity = {
+        x: 3,
+        y: 0,
+      };
+      this.invaders = [];
+  
+      const columns = Math.floor(Math.random() * 10 + 5);
+      const rows = Math.floor(Math.random() * 10 + 2);
+      this.width = columns * 30;
+  
+      for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+          this.invaders.push(new Invader(i * 30, j * 30));
+        }
+      }
     }
-    
-        };
-    
-
-        
-
-
+  
+    update() {
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+      this.velocity.y = 0;
+      if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+        this.velocity.x = -this.velocity.x * 1.15;
+        this.velocity.y = 38;
+      }
+    }
+  }
+  
+  class Invader {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+  
+  // Exemplo de uso:
+  const grid = new Grid();
+  grid.update();
+  
