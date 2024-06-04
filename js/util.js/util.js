f36d8dea13f4a5f4ce08f5dbed413c31c8255a02
Clayton1224 <claytonrichard153@gmail.com>
2024-06-04 16:32:29 -0400

    util

---
+++ js/util.js/util.js
@@ -0,0 +1,23 @@
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function createScoreLabel({ score = 100, object }) {
+    const scoreLabel = document.createElement("label");
+    scoreLabel.innerHTML = score;
+    scoreLabel.style.position = "absolute";
+    scoreLabel.style.color = "white";
+    scoreLabel.style.top = object.position.y + "px";
+    scoreLabel.style.left = object.position.x + "px";
+    scoreLabel.style.userSelect = "none";
+    document.querySelector("#parentDiv").appendChild(scoreLabel);
+    
+    gsap.to(scoreLabel, {
+        opacity: 0,
+        y: -30,
+        duration: 0.75,
+        onComplete: () => {
+            scoreLabel.parentNode.removeChild(scoreLabel);
+        }
+    });
+}
