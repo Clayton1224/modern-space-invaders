f3be1464112a63aced06bce38e75ecd2055ef036
Clayton1224 <claytonrichard153@gmail.com>
2024-06-06 14:26:32 -0400

    add int function in index.js file

--- index.js
+++ index.js
@@ -36,3 +36,47 @@ let score = 0;
 let spamBuffer = 500;
 let fps = 60;
 let fpsInterval =1000 / fps;
+
+function init(){
+    player = new player();
+    projectiles =[];
+    grids =[];
+    invaderProjectile =[];
+    particles =[];
+    bombs =[];
+    powerUps =[];
+    frames =0;
+    
+     keys = {
+        arrowLeft: { pressed: false },
+        arrowRight: { pressed: false },
+        space: { pressed: false }
+    };
+    
+    let randomInterval = Math.floor(Math.random() * 500 + 500);
+    let game = {
+        over: false,
+        active: true
+    };
+    
+    let score = 0;
+}
+
+function init() {
+    for (let i = 0; i < 100; i++) { // Adicionado o operador de comparação '<'
+        particles.push(   
+            new Particle({
+                position: {
+                    x: randomBetween(0, canvas.width),
+                    y: randomBetween(0, canvas.height),
+                },
+                velocity: {
+                    x: (Math.random() - 0.5) * 2,
+                    y: (Math.random() - 0.5) * 2,
+                },
+                color: "white",
+                fades: true,
+            })
+        );
+    }
+}
