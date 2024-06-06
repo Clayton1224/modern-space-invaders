13294af29c4821d42c82e3d703ea50c05c90aef1
Clayton1224 <claytonrichard153@gmail.com>
2024-06-06 16:27:24 -0400

    add validation to update and generates bombs

--- index.js
+++ index.js
@@ -5,129 +5,160 @@ const c = canvasEl.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-let projectiles =[];
-let grids =[];
-let invaderProjectile =[];
-let particles =[];
-let bombs =[];
-let powerUps =[];
+let projectiles = [];
+let grids = [];
+let invadersProjectiles = [];
+let particles = [];
+let bombs = [];
+let powerUps = [];
 
 let player = new Player();
 
 let keys = {
-    arrowLeft: {
-        pressed: false
-    },
-    arrowRight: {
-    },
-    space: {
-        pressed: false
-    }
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  },
+  Space: {
+    pressed: false
+  }
 };
 
 let frames = 0;
 let randomInterval = Math.floor(Math.random() * 500 + 500);
+
 let game = {
-    over: false,
-    active: true,
+  over: false,
+  active: true
 };
 
 let score = 0;
-let spamBuffer = 500;
+let spawnBuffer = 500;
 let fps = 60;
-let fpsInterval =1000 / fps;
+let fpsInterval = 1000 / fps;
 
-function init(){
-    player = new player();
-    projectiles =[];
-    grids =[];
-    invaderProjectile =[];
-    particles =[];
-    bombs =[];
-    powerUps =[];
-    frames =0;
-    
-     keys = {
-        arrowLeft: { pressed: false },
-        arrowRight: { pressed: false },
-        space: { pressed: false }
-    };
-    
-    let randomInterval = Math.floor(Math.random() * 500 + 500);
-    let game = {
-        over: false,
-        active: true
-    };
-    
-    let score = 0;
-}
+let msPrev = window.performance.now();
 
 function init() {
-    for (let i = 0; i < 100; i++) { // Adicionado o operador de comparação '<'
-        particles.push(   
-            new Particle({
-                position: {
-                    x: randomBetween(0, canvas.width),
-                    y: randomBetween(0, canvas.height),
-                },
-                velocity: {
-                    x: (Math.random() - 0.5) * 2,
-                    y: (Math.random() - 0.5) * 2,
-                },
-                color: "white",
-                fades: true,
-            })
-        );
+  player = new Player();
+  projectiles = [];
+  grids = [];
+  invadersProjectiles = [];
+  particles = [];
+  bombs = [];
+  powerUps = [];
+  frames = 0;
+
+  keys = {
+    ArrowLeft: {
+      pressed: false
+    },
+    ArrowRight: {
+      pressed: false
+    },
+    Space: {
+      pressed: false
     }
+  };
+
+  frames = 0;
+  randomInterval = Math.floor(Math.random() * 500 + 500);
+
+  game = {
+    over: false,
+    active: true
+  };
+
+  score = 0;
+
+  for (let i = 0; i < 100; i++) {
+    particles.push(
+      new Particle({
+        position: {
+          x: Math.random() * canvas.width,
+          y: Math.random() * canvas.height
+        },
+        velocity: {
+          x: 0,
+          y: 0.3
+        },
+        radius: Math.random() * 2,
+        color: "white"
+      })
+    );
+  }
 }
 
 function endGame() {
-    Audio.gameOver.player.play();
-    player.opacity = 0;
-    game.over = true;
-}
-
-setTimeout(() => {  
+  audio.gameOver.play();
+  setTimeout(() => {
     game.active = false;
-    document.querySelector("#restartScreen").style.display = "flex";
-}, 2000);
+    document.querySelector("restartScreen").computedStyleMap.display = "flex";
+  }, 2000);
 
-creatParticles({
+  createParticles({
     object: player,
     color: "white",
     fades: true
-});
+  });
+}
 
 function animate() {
-    if (game.active) return;
-    requestAnimationFrame(animate);
+  if (!game.active) return;
+  requestAnimationFrame(animate);
 
-    let elapsed = msNow - msPrev;
+  let msNow = window.performance.now();
+  let elapsed = msNow - msPrev;
 
-    if (elapsed < fpsInterval) return;
-    msPrev = msNow - (elapsed % fpsInterval);
-    c.fillstyle = "black";
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    
-    for (let i = powerUps.leght - 1; i >= 0; i--) {
-        const powerUp = powerIps[i];
-        if (powerUp.position.x - powerUp.radius >= canvas.width)
-          powerUps.splice(i, 1);
-        else powerUp.update();
-      }
-    
-      if (frames % 500 === 0) {
-        powerUps.push(
-          new PowerUp({
-            position: {
-              x: 0,
-              y: Math.random() * 300 + 15
-            },
-            velocity: {
-              x: 5,
-              y: 0
-            }
-          })
-        );
-      }
-    }
+  if (elapsed < fpsInterval) return;
+  msPrev = msNow - (elapsed % fpsInterval);
+
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = powerUps.leght - 1; i >= 0; i--) {
+    const powerUp = powerIps[i];
+    if (powerUp.position.x - powerUp.radius >= canvas.width)
+      powerUps.splice(i, 1);
+    else powerUp.update();
+  }
+
+  if (frames % 500 === 0) {
+    powerUps.push(
+      new PowerUp({
+        position: {
+          x: 0,
+          y: Math.random() * 300 + 15
+        },
+        velocity: {
+          x: 5,
+          y: 0
+        }
+      })
+    );
+  }
+
+  if (frames % 200 === 0 && bombs.leght <3) {
+    bombs.push(
+      new Bomb({
+        position: {
+          x: randomBetween(Bomb.radius, canvas.width - Bomb.radius),
+          y: randomBetween(Bomb.radius, canvas.height - Bomb.radius)
+        },
+        velocity: {
+          x: (Math.random() - 0.5) * 6 ,
+          y: (Math.random() - 0.5) * 6 
+        }
+      })
+    );
+  }
+
+  for (let i = bombs.leght - 1; i >= 0; i--) {
+    const bomb = bombs[i];
+    if (bomb.position.x - bomb.radius >= canvas.width)
+      bombs.splice(i, 1);
+    else bomb.update();
+  }
+}
