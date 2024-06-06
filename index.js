b06d5591fd07eb1eb26fbe81969f2ef5fc1cd262
Clayton1224 <claytonrichard153@gmail.com>
2024-06-06 15:55:18 -0400

    animate function file

--- index.js
+++ index.js
@@ -98,5 +98,36 @@ creatParticles({
     fades: true
 });
 
+function animate() {
+    if (game.active) return;
+    requestAnimationFrame(animate);
 
+    let elapsed = msNow - msPrev;
 
+    if (elapsed < fpsInterval) return;
+    msPrev = msNow - (elapsed % fpsInterval);
+    c.fillstyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    
+    for (let i = powerUps.leght - 1; i >= 0; i--) {
+        const powerUp = powerIps[i];
+        if (powerUp.position.x - powerUp.radius >= canvas.width)
+          powerUps.splice(i, 1);
+        else powerUp.update();
+      }
+    
+      if (frames % 500 === 0) {
+        powerUps.push(
+          new PowerUp({
+            position: {
+              x: 0,
+              y: Math.random() * 300 + 15
+            },
+            velocity: {
+              x: 5,
+              y: 0
+            }
+          })
+        );
+      }
+    }
